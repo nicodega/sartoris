c8dc7e7ef5ade4ecf8df06561b6dbd40e54acc00
nicodega <nicodega@619681a5-9619-0410-a131-ed93f25ce3d8>
2008-06-28 20:27:59 +0000

    useless

--- tags/sartoris_BMAYEM/arch/Li386emu/include/kernel-arch.h
+++
@@ -1,6 +0,0 @@
-#ifndef _KERNEL_ARCH_H_
-#define _KERNEL_ARCH_H_
-
-#define VALIDATE_PTR(x) (1)
-
-#endif
--- tags/sartoris_BMAYEM/include/lib/bitops.h
+++
@@ -1,9 +0,0 @@
-#ifndef _BITOPS_H_
-#define _BITOPS_H_
-
-#define BITMAP_SIZE(a) a / (sizeof(unsigned int) << 3)
-
-int getbit(unsigned int *array, int pos);
-void setbit(unsigned int *array, int pos, int value);
-
-#endif
