f36427c2d925a859bfb70c41b3140a25324c2359
nicodega <nicodega@users.noreply.github.com>
2013-01-01 21:33:37 +0000

    Now every syscall takes it's first three parameters on eax, edx and ecx

---
+++ sartoris/arch/i386/include/reg-calls.h
@@ -0,0 +1,24 @@
+
+#ifndef ARCH_FUNC_ATT0
+#define ARCH_FUNC_ATT0
+#endif
+
+#ifndef ARCH_FUNC_ATT1
+#define ARCH_FUNC_ATT1 __attribute__((regparm(1))) 
+#endif
+
+#ifndef ARCH_FUNC_ATT2
+#define ARCH_FUNC_ATT2 __attribute__((regparm(2))) 
+#endif
+
+#ifndef ARCH_FUNC_ATT3
+#define ARCH_FUNC_ATT3 __attribute__((regparm(3))) 
+#endif
+
+#ifndef ARCH_FUNC_ATT4
+#define ARCH_FUNC_ATT4 __attribute__((regparm(3)))
+#endif
+
+#ifndef ARCH_FUNC_ATT5
+#define ARCH_FUNC_ATT5 __attribute__((regparm(3)))
+#endif
